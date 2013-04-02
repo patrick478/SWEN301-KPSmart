3616650f4ccdb09a1fee8384fe2081a56fe88f8c
Ben Anderson <benanderson@acidic.co.nz>
2013-04-02 13:31:23 +1300

    All the stuff

---
+++ Common/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class Config
+    {
+        public Config(string path)
+        {
+
+        }
+
+        public object Get(string name)
+        {
+            return null;
+        }
+
+        public object Get(string name, object def)
+        {
+            return null;
+        }
+    }
+}
