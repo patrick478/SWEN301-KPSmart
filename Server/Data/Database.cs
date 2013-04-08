cc86e7fe0bb5b46ee75a2eb0c6c384b4816027fa
Ben Anderson <benanderson@acidic.co.nz>
2013-04-08 19:33:48 +1200

    We now have automatic table creation.. explanation and comments coming soon..

--- Server/Data/Database.cs
+++ Server/Data/Database.cs
@@ -1,11 +1,18 @@
 ﻿using System.Data.SQLite;
 using Server.Gui;
+using System.Collections.Generic;
 
 namespace Server.Data
 {
     public class Database
     {
         private SQLiteConnection connection;
+        private Dictionary<string, string> tables = new Dictionary<string, string>();
+
+        public Database()
+        {
+            tables.Add("syslog", "CREATE TABLE syslog (id int, body text, time timestamp)");
+        }
 
         public static void CreateDatabase()
         {
@@ -21,6 +28,28 @@ namespace Server.Data
             this.connection.Open();
 
             Logger.WriteLine("Connected to database");
+
+            // Perform table existance checks.
+            foreach(KeyValuePair<string, string> kvp in tables)
+            {
+                var sql = string.Format("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='{0}';", kvp.Key);
+                SQLiteCommand command = new SQLiteCommand(sql, this.connection);
+                object returnedValue = command.ExecuteScalar();
+
+                bool exists = false;
+                if (returnedValue == null)
+                    exists = false;
+                else
+                    if ((long)returnedValue > 0)
+                        exists = true;
+
+                if (!exists)
+                {
+                    Logger.WriteLine("Table {0} does not exist.. creating..", kvp.Key);
+                    command = new SQLiteCommand(kvp.Value, this.connection);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
