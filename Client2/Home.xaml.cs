8f9e1eca3f87a06636cf6ca45dec6fcd92459954
Patrick Barnes <patrick478@gmail.com>
2013-04-09 11:18:33 +1200

    Added initial GUI framework
    Changed the project type of the GUI from a Windows form to a Windows
    Performance Framework project, and added some initial (rough) GUI stuff.

---
+++ Client2/Home.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void RoutesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            RoutesPage routes = new RoutesPage();
+            this.NavigationService.Navigate(routes);
+
+        }
+    }
+}
