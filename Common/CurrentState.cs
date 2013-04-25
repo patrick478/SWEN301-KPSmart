8f45d9af03a4eb4f34f7488a8ffdac3a641d051f
Isabel Broome-Nicholson <isabel.broomenicholson@gmail.com>
2013-04-25 17:18:05 +1200

    moved Initialisation of CurrentState collections methods to CurrentState from State

--- Common/CurrentState.cs
+++ Common/CurrentState.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Common
 {
@@ -25,6 +27,30 @@ namespace Common
         {
             routes.Remove(id);
         }
+
+        /// <summary>
+        /// Initialises the state with a set of active routes in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="routes">the current routes</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'routes' is null</exception>
+        public void InitialiseRoutes(IDictionary<int, Route> routes)
+        {
+            if (this.routes != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if routes is already initialised.");
+            }
+
+            if (routes == null)
+            {
+                throw new ArgumentException("Cannot set all routes to null.", "routes");
+            }
+
+            this.routes = new ConcurrentDictionary<int, Route>(routes);
+        }
         #endregion
 
 
@@ -37,6 +63,31 @@ namespace Common
         public void RemoveRouteNode(int id)
         {
             routeNodes.Remove(id);
+        }
+
+        /// <summary>
+        /// Initialises the state with a set of active routeNodes in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="routeNodes">the current routeNodes</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'routeNodes' is null</exception>
+        public void InitialiseRouteNodes(IDictionary<int, RouteNode> routeNodes)
+        {
+            if (this.routeNodes != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if routeNodes is already initialised.");
+            }
+
+            if (routeNodes == null)
+            {
+                throw new ArgumentException("Cannot set all routeNodes to null.", "routeNodes");
+            }
+
+            this.routeNodes = new ConcurrentDictionary<int, RouteNode>(routeNodes);
+
         }
         #endregion
 
@@ -52,6 +103,31 @@ namespace Common
             prices.Remove(id);
 
         }
+
+        /// <summary>
+        /// Initialises the state with a set of active prices in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="prices">the current prices</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'prices' is null</exception>
+        public void InitialisePrices(IDictionary<int, Price> prices)
+        {
+
+            if (this.prices != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if prices is already initialised.");
+            }
+
+            if (prices == null)
+            {
+                throw new ArgumentException("Cannot set all prices to null.", "prices");
+            }
+
+            this.prices = new ConcurrentDictionary<int, Price>(prices);
+        }
         #endregion
 
 
@@ -64,6 +140,30 @@ namespace Common
         public void RemoveCountry(int id)
         {
             countries.Remove(id);
+        }
+
+        /// <summary>
+        /// Initialises the state with a set of active countries in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="countries">the current countries</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'countries' is null</exception>
+        public void InitialiseCountries(IDictionary<int, Country> countries)
+        {
+            if (this.countries != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if countries is already initialised.");
+            }
+
+            if (countries == null)
+            {
+                throw new ArgumentException("Cannot set all countries to null.", "countries");
+            }
+
+            this.countries = new ConcurrentDictionary<int, Country>(countries);
         }
         #endregion
 
@@ -78,6 +178,30 @@ namespace Common
         {
             deliveries.Remove(id);
         }
+
+        /// <summary>
+        /// Initialises the state with a set of active deliveries in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="deliveries">the current deliveries</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'deliveries' is null</exception>
+        public void InitialiseDeliveries(IDictionary<int, Delivery> deliveries)
+        {
+            if (this.deliveries != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if deliveries is already initialised.");
+            }
+
+            if (deliveries == null)
+            {
+                throw new ArgumentException("Cannot set all deliveries to null.", "deliveries");
+            }
+
+            this.deliveries = new ConcurrentDictionary<int, Delivery>(deliveries);
+        }
         #endregion
 
 
@@ -90,6 +214,30 @@ namespace Common
         public void RemoveCompany(int id)
         {
             companies.Remove(id);
+        }
+
+        /// <summary>
+        /// Initialises the state with a set of active companies in the system. This can only be done once, and must happen
+        /// before other methods are used.
+        /// 
+        /// The given collection is copied into the internal fields of State.
+        /// </summary>
+        /// <param name="companies">the current companies</param>
+        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
+        /// <exception cref="ArgumentException">if 'companies' is null</exception>
+        public void InitialiseCompanies(IDictionary<int, Company> companies)
+        {
+            if (this.companies != null)
+            {
+                throw new IllegalActionException("Cannot set all routes if companies is already initialised.");
+            }
+
+            if (companies == null)
+            {
+                throw new ArgumentException("Cannot set all companies to null.", "companies");
+            }
+
+            this.companies = new ConcurrentDictionary<int, Company>(companies);
         }
         #endregion
 
--- Common/State.cs
+++ Common/State.cs
@@ -46,31 +46,6 @@ namespace Common
         {
             return new List<Route>(routes.Values);
         }
-
-        /// <summary>
-        /// Initialises the state with a set of active routes in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="routes">the current routes</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'routes' is null</exception>
-        public void InitialiseRoutes(IDictionary<int, Route> routes)
-        {
-            if (this.routes != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if routes is already initialised.");
-            }
-
-            if (routes == null)
-            {
-                throw new ArgumentException("Cannot set all routes to null.", "routes");
-            }
-
-            this.routes = new ConcurrentDictionary<int, Route>(routes);
-        }
-
         #endregion
 
 
@@ -83,31 +58,6 @@ namespace Common
         public IEnumerable<RouteNode> GetAllRouteNodes()
         {
             return new List<RouteNode>(routeNodes.Values);
-        }
-
-        /// <summary>
-        /// Initialises the state with a set of active routeNodes in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="routeNodes">the current routeNodes</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'routeNodes' is null</exception>
-        public void InitialiseRouteNodes(IDictionary<int, RouteNode> routeNodes)
-        {
-            if (this.routeNodes != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if routeNodes is already initialised.");
-            }
-
-            if (routeNodes == null)
-            {
-                throw new ArgumentException("Cannot set all routeNodes to null.", "routeNodes");
-            }
-            
-            this.routeNodes = new ConcurrentDictionary<int, RouteNode>(routeNodes);
-            
         }
         #endregion
 
@@ -122,31 +72,6 @@ namespace Common
         {
             return new List<Price>(prices.Values);
         }
-
-        /// <summary>
-        /// Initialises the state with a set of active prices in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="prices">the current prices</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'prices' is null</exception>
-        public void InitialisePrices(IDictionary<int, Price> prices)
-        {
-
-            if (this.prices != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if prices is already initialised.");
-            }
-
-            if (prices == null)
-            {
-                throw new ArgumentException("Cannot set all prices to null.", "prices");
-            }
-            
-            this.prices = new ConcurrentDictionary<int, Price>(prices); 
-        }
         #endregion
 
         #region countries
@@ -159,31 +84,6 @@ namespace Common
         {
             return new List<Country>(countries.Values);
         }
-
-        /// <summary>
-        /// Initialises the state with a set of active countries in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="countries">the current countries</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'countries' is null</exception>
-        public void InitialiseCountries(IDictionary<int, Country> countries)
-        {
-            if (this.countries != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if countries is already initialised.");
-            }
-
-            if (countries == null)
-            {
-                throw new ArgumentException("Cannot set all countries to null.", "countries");
-            }
-
-            this.countries = new ConcurrentDictionary<int, Country>(countries);           
-        }
-
         #endregion
 
 
@@ -197,31 +97,6 @@ namespace Common
         {
             return new List<Delivery>(deliveries.Values);
         }
-
-        /// <summary>
-        /// Initialises the state with a set of active deliveries in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="deliveries">the current deliveries</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'deliveries' is null</exception>
-        public void InitialiseDeliveries(IDictionary<int, Delivery> deliveries)
-        {
-            if (this.deliveries != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if deliveries is already initialised.");
-            }
-
-            if (deliveries == null)
-            {
-                throw new ArgumentException("Cannot set all deliveries to null.", "deliveries");
-            }
-            
-            this.deliveries = new ConcurrentDictionary<int, Delivery>(deliveries);      
-        }
-
         #endregion
 
 
@@ -236,29 +111,7 @@ namespace Common
             return new List<Company>(companies.Values);
         }
 
-        /// <summary>
-        /// Initialises the state with a set of active companies in the system. This can only be done once, and must happen
-        /// before other methods are used.
-        /// 
-        /// The given collection is copied into the internal fields of State.
-        /// </summary>
-        /// <param name="companies">the current companies</param>
-        /// <exception cref="IllegalActionException">if has already been initialised.</exception>
-        /// <exception cref="ArgumentException">if 'companies' is null</exception>
-        public void InitialiseCompanies(IDictionary<int, Company> companies)
-        {
-            if (this.companies != null)
-            {
-                throw new IllegalActionException("Cannot set all routes if companies is already initialised.");
-            }
 
-            if (companies == null)
-            {
-                throw new ArgumentException("Cannot set all companies to null.", "companies");
-            }
-            
-            this.companies = new ConcurrentDictionary<int, Company>(companies);        
-        }
         #endregion
     }
 }
