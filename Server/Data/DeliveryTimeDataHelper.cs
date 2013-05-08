a045dea74438496a505a88dd376410ba8191bb6e
Isabel Broome-Nicholson <isabel.broomenicholson@gmail.com>
2013-05-08 12:07:12 +1200

    implemented Create for DeliveryTimeDataHelper

--- Server/Data/DeliveryTimeDataHelper.cs
+++ Server/Data/DeliveryTimeDataHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Common;
+using Server.Gui;
 
 namespace Server.Data
 {
@@ -22,17 +23,55 @@ namespace Server.Data
             throw new NotImplementedException();
         }
 
-        public void Update (int route_id, IList<WeeklyTime> departureTimes)
+        public void Update (int route_id, int event_id, IList<WeeklyTime> departureTimes)
         {
             throw new NotImplementedException();
         }
 
-        public void Create (int route_id, IList<WeeklyTime> departureTimes)
+        /// <summary>
+        /// Saves all the departure times for the new route.
+        /// </summary>
+        /// <param name="route_id">the id of the newly created route</param>
+        /// <param name="event_id">the event id of creating the route</param>
+        /// <param name="departureTimes">the departure times to save</param>
+        public void Create (int route_id, int event_id, IList<WeeklyTime> departureTimes)
         {
-            throw new NotImplementedException();
+            if (route_id == 0)
+                throw new ArgumentException("Route_id cannot be zero", "route_id");
+
+            if (departureTimes == null)
+                throw new ArgumentException("departureTimes cannot be null", "departureTimes");
+
+            // BEGIN LOCK HERE
+            lock (Database.Instance)
+            {
+                // check that no entries for that route_id
+                var sql = SQLQueryBuilder.SelectFieldsWhereFieldEquals(TABLE_NAME, "route_id", route_id.ToString(), new string[] { ID_COL_NAME });
+                var rows = Database.Instance.FetchRows(sql);
+                if (rows.Length != 0)
+                    throw new DatabaseException("That route already has active departure time entries.  Should use Update method instead.");
+
+                // save all the entries
+                foreach (WeeklyTime departureTime in departureTimes) 
+                {
+                    sql = SQLQueryBuilder.CreateNewRecord(TABLE_NAME, ID_COL_NAME, new[] { EVENT_ID, "route_id", "weekly_time" }, new string[] { event_id.ToString(), route_id.ToString(), departureTime.Value.Ticks.ToString() });
+                    Database.Instance.InsertQuery(sql);
+                }    
+            }
+            // END LOCK HERE
+
+            Logger.WriteLine("Saved {0} departure times for new route: {1}", departureTimes.Count, route_id);
         }
 
-        public int GetId (int route_id, WeeklyTime departureTime)
+        /// <summary>
+        /// Returns the departure_time_id of the given combination.
+        /// 
+        /// Returns 0 if it doesn't exist.
+        /// </summary>
+        /// <param name="route_id"></param>
+        /// <param name="departureTime"></param>
+        /// <returns></returns>
+        private int GetId (int route_id, WeeklyTime departureTime)
         {
             throw new NotImplementedException();
         }
