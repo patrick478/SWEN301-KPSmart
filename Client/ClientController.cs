b9ef704d56151882d43b83c6d407fd73920f491a
adamswen <mclareadam@myvuw.ac.nz>
2013-05-10 13:57:26 +1200

    Added documentation to all currently usable methods in ClientController.

--- Client/ClientController.cs
+++ Client/ClientController.cs
@@ -47,6 +47,10 @@ namespace Client
             }
         }
 
+        /// <summary>
+        /// Updates the Client's state with an Add/Edit of a DataObject.
+        /// </summary>
+        /// <param name="tokens">Network Message.</param>
         private void ObjectUpdate(string[] tokens)
         {
             int count = 1;
@@ -74,6 +78,10 @@ namespace Client
             }
         }
 
+        /// <summary>
+        /// Deletes the encoded DataObject from the Client's state.
+        /// </summary>
+        /// <param name="tokens">Network Message.</param>
         private void ObjectDelete(string[] tokens)
         {
             int count = 1;
@@ -99,16 +107,30 @@ namespace Client
         #region Sending (Triggers Called by GUI)
 
         #region Delivery
+        /// <summary>
+        /// Requests the server to provide options for a new Delivery to be made.
+        /// </summary>
+        /// <param name="originId">ID of Origin Location.</param>
+        /// <param name="destinationId">ID of Destination Location.</param>
+        /// <param name="weight">Weight (in grams) of the package.</param>
+        /// <param name="volume">Volume (in cm^3) of the package.</param>
         public void RequestDelivery(int originId, int destinationId, int weight, int volume)
         {
             Send(NetCodes.CL_DELIVERY_REQUEST, Convert.ToString(originId), Convert.ToString(destinationId), Convert.ToString(weight), Convert.ToString(volume));
         }
 
+        /// <summary>
+        /// Selects a Delivery option from the provided Types of the outstanding request.
+        /// </summary>
+        /// <param name="type">PathType to use.</param>
         public void ChooseDelivery(PathType type)
         {
             Send(NetCodes.CL_DELIVERY_SELECT, type.ToNetString());
         }
 
+        /// <summary>
+        /// Cancels an outstanding Delivery Request.
+        /// </summary>
         public void CancelDelivery()
         {
             Send(NetCodes.CL_DELIVERY_SELECT, NetCodes.PATH_CANCEL);
@@ -116,16 +138,30 @@ namespace Client
         #endregion
 
         #region Country
+        /// <summary>
+        /// Add a new Country.
+        /// </summary>
+        /// <param name="code">3-character country code.</param>
+        /// <param name="name">Country name.</param>
         public void AddCountry(string code, string name)
         {
             Send(NetCodes.CL_OBJECT_ADD, NetCodes.OBJECT_COUNTRY, code, name);
         }
 
+        /// <summary>
+        /// Edit the code of an existing Country.
+        /// </summary>
+        /// <param name="id">ID of Country to edit.</param>
+        /// <param name="code">New 3-character country code to use.</param>
         public void EditCountry(int id, string code)
         {
             Send(NetCodes.CL_OBJECT_EDIT, NetCodes.OBJECT_COUNTRY, code);
         }
 
+        /// <summary>
+        /// Delete an existing Country.
+        /// </summary>
+        /// <param name="id">ID of Country to delete.</param>
         public void DeleteCountry(int id)
         {
             Send(NetCodes.CL_OBJECT_DELETE, Convert.ToString(id), NetCodes.OBJECT_COUNTRY);
@@ -133,11 +169,19 @@ namespace Client
         #endregion
 
         #region Company
+        /// <summary>
+        /// Add a new Company.
+        /// </summary>
+        /// <param name="name">Company name.</param>
         public void AddCompany(string name)
         {
             Send(NetCodes.CL_OBJECT_ADD, NetCodes.OBJECT_COMPANY, name);
         }
 
+        /// <summary>
+        /// Delete an existing Company.
+        /// </summary>
+        /// <param name="id">ID of Company to delete.</param>
         public void DeleteCompany(int id)
         {
             Send(NetCodes.CL_OBJECT_DELETE, Convert.ToString(id), NetCodes.OBJECT_COMPANY);
@@ -145,16 +189,34 @@ namespace Client
         #endregion
 
         #region Price
+        /// <summary>
+        /// Add a new Customer Price.
+        /// </summary>
+        /// <param name="originId">ID of Origin Location.</param>
+        /// <param name="destinationId">ID of Destination Location.</param>
+        /// <param name="priority">Priority this Price applies to.</param>
+        /// <param name="weightPrice">Cost per Gram.</param>
+        /// <param name="volumePrice">Cost per cm^3.</param>
         public void AddPrice(int originId, int destinationId, Priority priority, int weightPrice, int volumePrice)
         {
             Send(NetCodes.CL_OBJECT_ADD, NetCodes.OBJECT_PRICE, Convert.ToString(originId), Convert.ToString(destinationId), priority.ToNetString(), Convert.ToString(weightPrice), Convert.ToString(volumePrice));
         }
 
+        /// <summary>
+        /// Edit an existing Customer Price.
+        /// </summary>
+        /// <param name="id">ID of Price to edit.</param>
+        /// <param name="weightPrice">New Cost per Gram.</param>
+        /// <param name="volumePrice">New Cost per cm^3.</param>
         public void EditPrice(int id, int weightPrice, int volumePrice)
         {
             Send(NetCodes.CL_OBJECT_EDIT, Convert.ToString(id), NetCodes.OBJECT_PRICE, Convert.ToString(weightPrice), Convert.ToString(volumePrice));
         }
 
+        /// <summary>
+        /// Delete an existing Customer Price.
+        /// </summary>
+        /// <param name="id">ID of Price to delete.</param>
         public void DeletePrice(int id)
         {
             Send(NetCodes.CL_OBJECT_DELETE, Convert.ToString(id), NetCodes.OBJECT_PRICE);
@@ -175,6 +237,10 @@ namespace Client
             // *Cost/gram (int) - *Cost/cm3 (int) - *Max Weight (int) - *Max Capacity (int) - *Trip Duration (int) - *Trip Times 
         }
 
+        /// <summary>
+        /// Delete an existing Route.
+        /// </summary>
+        /// <param name="id">ID of Route to delete.</param>
         public void DeleteRoute(int id)
         {
             Send(NetCodes.CL_OBJECT_DELETE, Convert.ToString(id), NetCodes.OBJECT_ROUTE);
