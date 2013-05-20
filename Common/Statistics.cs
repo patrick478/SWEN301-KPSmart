39d9d6736072061440f84438d22cf4f52134f106
Therrius <joshua.scott.132@gmail.com>
2013-05-20 18:58:16 +1200

    Statitics fleashed out

--- Common/Statistics.cs
+++ Common/Statistics.cs
@@ -14,40 +14,61 @@ namespace Common
             this.state = state;
         }
 
+        public int TotalRevenue
+        {
+            get;
+            private set;
+        }
+
+        public int TotalExpenditure
+        {
+            get;
+            private set;
+        }
+
+        public int TotalEvents
+        {
+            get;
+            private set;
+        }
+
+        public List<Triple> Triples
+        {
+            get;
+            private set;
+        }
+
+
         public void GetStatistics(){
 
             IList<Delivery> deliveries = state.GetAllDeliveries();
+            Triple.Clear();
 
-            //Total Revenue - Total revenue from Customer charges.
-            //Total Expenditure - Total cost of transport charges.
             //Total Events - The number of events used to generate these figures.
-            long totalRevenue = 0;
-            long totalExpenditure = 0;
-            long totalEvents = deliveries.Count;
+            TotalEvents = deliveries.Count;
 
             foreach (Delivery delivery in deliveries)
             {
-                totalRevenue += delivery.TotalPrice;
-                totalExpenditure += delivery.TotalCost;
 
-
-                //create eideitefier
-                //delivery.Origin, delivery.Duration, delivery.Priority
+                //Total Revenue - Total revenue from Customer charges.
+                //Total Expenditure - Total cost of transport charges.
+                int revenue = delivery.TotalPrice - delivery.TotalCost;
+                TotalRevenue += delivery.TotalPrice - delivery.TotalCost;
+                TotalExpenditure += delivery.TotalCost;
 
                 Triple triple = Triple.GetTriple(delivery.Origin, delivery.Destination, delivery.Priority);
 
+                //Amount of Mail - Total number of mail, total weight, total volume. For each path.
+                triple.TotalMail++;
+                triple.TotalVolume += delivery.VolumeInCm3;
+                triple.TotalWeight += delivery.WeightInGrams;
 
+                //Average Delivery Times - Average delivery time for each Triple*.
+                triple.TotalDeliveryTimes.Add(delivery.Duration);
+
+                //Critical Routes - List of Triples* where the average transport cost is more than the average customer price.
+                triple.TotalRevenue += revenue;
             }
-
-            //Amount of Mail - Total number of mail, total weight, total volume. For each path.
-
-            //Average Delivery Times - Average delivery time for each Triple*.
-
-            //Critical Routes - List of Triples* where the average transport cost is more than the average customer price.
-
-
-
-
         }
 
 
@@ -77,11 +98,40 @@ namespace Common
                 return newTriple;
             }
 
+            public static void Clear()
+            {
+                instances = new List<Triple>();
+            }
+
+            public int TotalMail { get; set; }
+            public int TotalWeight { get; set; }
+            public int TotalVolume { get; set; }
+
+            public int AverageDeliveryTimes
+            {
+                get
+                {
+                    double time = TotalDeliveryTimes.TotalMinutes;
+                    double num = TotalMail;
+                    return (int)(time / ((num > 0) ? num: 0));
+                }
+            }
+            public bool CriticalRoutes
+            {
+                get
+                {
+                    return TotalRevenue > 0;
+                }
+            }
+
+            public TimeSpan TotalDeliveryTimes { get; set; }
+            public int TotalRevenue { get; set; }
+
             public List<Triple> AllInstances
             {
                 get
                 {
-                    return instances;
+                    return new List<Triple>(instances);
                 }
             }
 
