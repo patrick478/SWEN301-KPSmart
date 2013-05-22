c624b62ebd210bc3f3132c765a1d5ca7fef679b0
Patrick Barnes <patrick478@gmail.com>
2013-05-23 01:15:08 +1200

    Added new dialog boxes

---
+++ Client/DialogBoxes/AddDistCenterDialogBox.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Client.DialogBoxes
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class AddDistCenterDialogBox : Window
+    {
+        public AddDistCenterDialogBox()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+           
+        }
+    }
+}
---
+++ Client/DialogBoxes/AddIntlPortDialogBox.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.Countries
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class AddIntlPortDialogBox : Window
+    {
+        public AddIntlPortDialogBox(ClientState client)
+        {
+            InitializeComponent();
+
+            foreach (var country in client.GetAllCountries())
+            {
+                //this.originComboBox.Items.Add(countries.Name);
+                ComboBoxItem cbi = new ComboBoxItem();
+                cbi.Content = country.Name;
+                cbi.Tag = country.ID;
+                this.countries.Items.Add(cbi);
+            }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+           
+        }
+    }
+}
