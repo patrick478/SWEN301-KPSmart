98c1c5127fdc242b1ab36bfd60a4b5a0537782dc
Isabel Broome-Nicholson <isabel.broomenicholson@gmail.com>
2013-05-23 22:54:22 +1200

    added domestic price to PriceDataHelper

---
+++ Common/DomesticPrice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// This class represents a price for a domestic route.
+    /// </summary>
+    public class DomesticPrice: Price
+    {
+
+        public DomesticPrice (Priority priority) {}
+
+
+        public override string ToString ()
+        {
+            return String.Format("DomesticPrice[ID={0}, Priority={1}, PricePerGram={2}, PricePerCm3={3}]", ID, Priority.ToString(), PricePerGram, PricePerCm3);
+        }
+
+    }
+}
